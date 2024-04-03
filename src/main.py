9693af37452795abded37546bfdb7954309e018b
Hiroaki Suzuki <rirazou05410@gmail.com>
2024-04-03 23:16:59 +0900

    静的チェックツールの微調整

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
+"""サンプルモジュール"""
+
 from typing import Generator
 
-
-def sum(a: int, b: int) -> int:
-    return a + b
+from sum import sum
 
 
 def fib(n: int) -> Generator[int, None, None]:
+    """フィボナッチ数列を生成するジェネレータ"""
     a, b = 0, 1
     while a < n:
         yield a
@@ -13,9 +14,18 @@ def fib(n: int) -> Generator[int, None, None]:
 
 
 def greeting(name: str) -> str:
+    """挨拶を返す
+
+    Args:
+        name (str): 名前
+
+    Returns:
+        str: "Hello " + name
+    """
     return "Hello " + name
 
 
 if __name__ == "__main__":
     print("Hello World!")
     print(sum(1, 2))
+    print(list(fib(10)))
---
+++ src/sum.py
@@ -0,0 +1,14 @@
+"""足し算を行う"""
+
+
+def sum(a: int, b: int) -> int:
+    """足し算を行う
+
+    Args:
+        a (int): 数値1
+        b (int): 数値2
+
+    Returns:
+        int: a + b
+    """
+    return a + b
